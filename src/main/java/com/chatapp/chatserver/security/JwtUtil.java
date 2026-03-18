38d76642fd60ce39318dd80ec8abef9b0d36f6a9
Pooja R <rpooja37r@gmail.com>
2026-03-18 23:32:58 +0530

    remove secrets from tracking

--- src/main/java/com/chatapp/chatserver/security/JwtUtil.java
+++ src/main/java/com/chatapp/chatserver/security/JwtUtil.java
@@ -3,18 +3,24 @@ package com.chatapp.chatserver.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
+import jakarta.annotation.PostConstruct; 
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private static final String SECRET =
-            "mysecretkeymysecretkeymysecretkeymysecretkey";
+	@Value("${jwt.secret}")
+	private String secret;
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+	private Key key;
+
+	@PostConstruct
+	public void init() {
+	    this.key = Keys.hmacShaKeyFor(secret.getBytes());
+	}
 
     public String extractUsername(String token) {
         return getClaims(token).getSubject();
